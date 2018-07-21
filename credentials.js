a7a73b867d0d42e57c268965573bfb075a0c811d
sbland93 <rltmqj@gmail.com>
2018-07-22 00:31:38 +0900

    180722

---
+++ credentials.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+
+	cookieSecret : 'haodoc king',
+
+
+	mongo: {
+		//기본 mlab주소.
+		development: {
+			connectionString : 'mongodb://haodoc:inspire0407@ds147391.mlab.com:47391/haodoc'
+		},
+		//deploy된 상태에서의 mongodb mlab 주소.
+		production: {
+			connectionString : 'mongodb://haodoc:inspire0407@ds147391.mlab.com:47391/haodoc'
+		},
+		test: {
+			connectionString: 'mongodb://haodoc:inspire0407@ds147391.mlab.com:47391/haodoc'
+		},
+		options : { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+
+        replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } },
+	}
+
+};
