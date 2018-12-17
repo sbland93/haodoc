5341332c483412c5d27a2ed967485b0b7119ca69
sbland93 <rltmqj@gmail.com>
2018-12-17 17:04:20 +0900

    181217

--- public/js/util/util.js
+++ public/js/util/util.js
@@ -1,12 +1,28 @@
 
 
+var baseFile = "/api/file";
 
+var getFiles = function(data){
+	return new Promise(function(resolve, reject){
+		$.ajax({
+			url: baseFile,
+			method: 'GET',
+			data: data,
+			success: function(rtnData){
+				resolve(rtnData);
+			},
+			fail: function(rtnData){
+				reject(rtnData);
+			},
+		});
+	});
+};
 
 
 //Vue.js 초기화 전용/ self.data_name = returnValue
-var util_vue_init = function(vue_self, which_func, data_name){
+var util_data_init = function(vue_self, which_func, data_name){
 			
-	which_func().then(function(_rtn){
+	which_func().then(function(_rtn){ 
 		vue_self[data_name] = _rtn;
 	}).catch(function(_rtn){
 		console.log(_rtn);
@@ -14,11 +30,13 @@ var util_vue_init = function(vue_self, which_func, data_name){
 
 }
 
-var init_event = function(self){
-	util_vue_init(self, getEvents, "events")
-}
 
-var init_coupon = function(self){
-	util_vue_init(self, getCoupons, "coupons");
-}
+var util_file_init = function(vue_self, dirString, data_name){
 
+	getFiles({dir : dirString}).then(function(_rtn){
+		vue_self[data_name] = _rtn;
+	}).catch(function(_rtn){
+		console.log(_rtn);
+	});
+
+}
